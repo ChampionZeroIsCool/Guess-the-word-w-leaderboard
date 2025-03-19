2a35b74fa3f0a16442538b7afa59bab9d6f18ccf
Ben Jameson <benjamesonn@gmail.com>
2025-03-19 18:59:27 +1100

    nav working

--- src/SidebarLayout.jsx
+++ src/SidebarLayout.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
   Dialog,
   DialogBackdrop,
   DialogPanel,
   TransitionChild,
-} from '@headlessui/react'
+} from "@headlessui/react";
 import {
   Bars3Icon,
   XMarkIcon,
   GlobeAsiaAustraliaIcon,
   PuzzlePieceIcon,
   HomeIcon,
-} from '@heroicons/react/24/outline'
+} from "@heroicons/react/24/outline";
 const navigation = [
-  { name: 'Home', href: '#', icon: HomeIcon, current: true },
-  { name: 'Game', href: '#', icon: PuzzlePieceIcon, current: false },
-  { name: 'Leaderboard', href: '#', icon: GlobeAsiaAustraliaIcon, current: false },
-]
+  { name: "Home", href: "#", icon: HomeIcon, current: true },
+  { name: "Game", href: "#", icon: PuzzlePieceIcon, current: false },
+  {
+    name: "Leaderboard",
+    href: "#",
+    icon: GlobeAsiaAustraliaIcon,
+    current: false,
+  },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function SidebarLayout() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-  const [currentNav, setCurrentNav] = useState('Game')
-  console.log(currentNav)
-
-  
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [currentNav, setCurrentNav] = useState("Home");
+  console.log(currentNav);
 
   return (
     <>
@@ -40,7 +43,11 @@ export default function SidebarLayout() {
         ```
       */}
       <div>
-        <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 lg:hidden">
+        <Dialog
+          open={sidebarOpen}
+          onClose={setSidebarOpen}
+          className="relative z-50 lg:hidden"
+        >
           <DialogBackdrop
             transition
             className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
@@ -53,16 +60,22 @@ export default function SidebarLayout() {
             >
               <TransitionChild>
                 <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
-                  <button type="button" onClick={() => setSidebarOpen(false)} className="-m-2.5 p-2.5">
+                  <button
+                    type="button"
+                    onClick={() => setSidebarOpen(false)}
+                    className="-m-2.5 p-2.5"
+                  >
                     <span className="sr-only">Close sidebar</span>
-                    <XMarkIcon aria-hidden="true" className="size-6 text-white" />
+                    <XMarkIcon
+                      aria-hidden="true"
+                      className="size-6 text-white"
+                    />
                   </button>
                 </div>
               </TransitionChild>
               {/* Sidebar component, swap this element with another sidebar if you like */}
               <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
-                <div className="flex h-1 shrink-0 items-center">
-                </div>
+                <div className="flex h-1 shrink-0 items-center"></div>
                 <nav className="flex flex-1 flex-col">
                   <ul role="list" className="flex flex-1 flex-col gap-y-7">
                     <li>
@@ -71,17 +84,20 @@ export default function SidebarLayout() {
                           <li key={item.name}>
                             <button
                               onClick={() => {
-                                setCurrentNav(item.name)
-                                setSidebarOpen(false)
+                                setCurrentNav(item.name);
+                                setSidebarOpen(false);
                               }}
                               className={classNames(
                                 item.name === currentNav
-                                  ? 'bg-gray-800 text-white'
-                                  : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-                                'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                                  ? "bg-gray-800 text-white"
+                                  : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                                "group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold"
                               )}
                             >
-                              <item.icon aria-hidden="true" className="size-6 shrink-0" />
+                              <item.icon
+                                aria-hidden="true"
+                                className="size-6 shrink-0"
+                              />
                               {item.name}
                             </button>
                           </li>
@@ -99,8 +115,7 @@ export default function SidebarLayout() {
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
-            <div className="flex h-1 shrink-0 items-center">
-            </div>
+            <div className="flex h-1 shrink-0 items-center"></div>
             <nav className="flex flex-1 flex-col">
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
                 <li>
@@ -108,17 +123,18 @@ export default function SidebarLayout() {
                     {navigation.map((item) => (
                       <li key={item.name}>
                         <button
-                          onClick={() =>
-                            setCurrentNav(item.name)
-                          }
+                          onClick={() => setCurrentNav(item.name)}
                           className={classNames(
                             item.name === currentNav
-                              ? 'bg-gray-800 text-white'
-                              : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-                            'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                              ? "bg-gray-800 text-white"
+                              : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                            "group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold"
                           )}
                         >
-                          <item.icon aria-hidden="true" className="size-6 shrink-0" />
+                          <item.icon
+                            aria-hidden="true"
+                            className="size-6 shrink-0"
+                          />
                           {item.name}
                         </button>
                       </li>
@@ -132,7 +148,11 @@ export default function SidebarLayout() {
 
         <div className="lg:pl-72">
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-            <button type="button" onClick={() => setSidebarOpen(true)} className="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+            <button
+              type="button"
+              onClick={() => setSidebarOpen(true)}
+              className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+            >
               <span className="sr-only">Open sidebar</span>
               <Bars3Icon aria-hidden="true" className="size-6" />
             </button>
@@ -140,16 +160,15 @@ export default function SidebarLayout() {
 
           <main className="py-10">
             <div className="px-4 sm:px-6 lg:px-8">
-            { currentNav === 'Home' ?
-                null/*home components*/ : null }
-              { currentNav === 'Game' ?
-                  null/*game components*/ : null }
-                  { currentNav === 'Leaderboard' ?
-                    null/*leaderboard components*/ : null }
+              {currentNav === "Home" ? null /*home components*/ : null}
+              {currentNav === "Game" ? null /*game components*/ : null}
+              {currentNav === "Leaderboard"
+                ? null /*leaderboard components*/
+                : null}
             </div>
           </main>
         </div>
       </div>
     </>
-  )
+  );
 }
